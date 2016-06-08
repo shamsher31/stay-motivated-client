a91ab290c60b33ef91c5d1aa1573b66221757fc1
shamsher31 <ansarisam47@gmail.com>
2016-06-08 11:26:23 +0530

    Add list of qoutes and display list using ngFor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,53 @@
 import { Component } from '@angular/core';
 
+export class Qoute {
+  title: string;
+  author: string;
+}
+
 @Component({
   selector: 'qoutes-app',
   template: `
-    <p>{{title}}</p>
-    <p>{{author}}</p>`
+    <div *ngIf="selectedQoute">
+      <p>{{selectedQoute.title}}</p>
+      <p>{{selectedQoute.author}}</p>
+      <input [(ngModel)]="selectedQoute.author">
+    </div>
+    <ul>
+      <li *ngFor="let qoute of qoutes" (click)="onSelect(qoute)">
+        <p>{{qoute.title}}</p>
+        <p>{{qoute.author}}</p>
+      </li>
+    </ul>`
 })
 
 export class AppComponent {
-  title = 'Love what you do, Do what you love.';
-  author = 'Steve Jobs';
-} 
+  public qoutes = QOUTES;
+  selectedQoute: Qoute;
+  onSelect(qoute: Qoute) {
+    this.selectedQoute = qoute;
+  }
+}
+
+var QOUTES: Qoute[] = [
+  {
+    title: 'Love what you do, Do what you love.',
+    author: 'Steve Jobs'
+  },
+  {
+    title: 'Work hard dream big.',
+    author: 'Steve Jobs'
+  },
+  {
+    title: 'Hardwork beats talent.',
+    author: 'Steve Jobs'
+  },
+  {
+    title: 'Life id too short to wait.',
+    author: 'Steve Jobs'
+  },
+  {
+    title: 'Never stop dreaming.',
+    author: 'Steve Jobs'
+  },
+]
